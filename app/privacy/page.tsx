06cf99c0283760406ed902069d6d62276cfefc1e
Sumit uniyal <suniyal141@gmail.com>
2025-11-23 12:51:45 +0530

    privacy

---
+++ app/privacy/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+      <p>
+        This app is for personal testing and development. We do not collect or
+        store any user data.
+      </p>
+    </>
+  );
+};
+
+export default page;
